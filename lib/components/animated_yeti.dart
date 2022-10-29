956b94f09801d72a32e25154286660e2697a166c
Louie Weng <louieweng@dhcp-206-87-134-156.ubcsecure.wireless.ubc.ca>
2022-10-29 12:47:28 -0700

    initial commit with flutter bootstrap

---
+++ lib/components/animated_yeti.dart
@@ -0,0 +1,13 @@
+// import 'package:flame/components.dart';
+
+// class AnimatedYeti extends SpriteAnimationComponent with HasGameRef {
+//   AnimatedYeti() : super(position: Vector2.all(32), size: Vector2.all(100)) {
+//     debugMode = true;
+//   }
+
+
+//   @override
+//   Future<void> onLoad() async {
+    
+//   }
+// }
