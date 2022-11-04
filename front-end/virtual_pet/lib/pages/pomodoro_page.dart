fdc7876f4dc87c9276681b139fe8c02cf0728620
Jacob Wu <99063771+wu-jacob@users.noreply.github.com>
2022-11-04 16:58:37 -0700

    added navigation to pomodoro timer page

---
+++ front-end/virtual_pet/lib/pages/pomodoro_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+/// pomodoro page (pomodoro timer)
+class PomodoroPage extends StatefulWidget {
+  const PomodoroPage({super.key});
+
+  @override
+  PomodoroState createState() => PomodoroState();
+}
+
+/// the state of pomodoro page (timer)
+class PomodoroState extends State<PomodoroPage> {
+  static const String routeName = "/PomodoroPage";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        /// displays the "Pomodoro Timer" message at the top
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).canvasColor,
+          elevation: 1.0,
+          title: const Text(
+            "Pomodoro Timer",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        body: Container(
+          child: const Text(
+            "Pomodoro timer coming soon...",
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 40.0,
+            ),
+          ),
+        ));
+  }
+}
