b09751374f68b98dab42a58ec92381c621c54613
Eric <summer.eric1@gmail.com>
2022-11-26 12:23:46 -0800

    addressed flutter analyze

--- front-end/virtual_pet/lib/pages/minutes.dart
+++ front-end/virtual_pet/lib/pages/minutes.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
 
 class MyMinutes extends StatelessWidget {
-  int mins;
+  final int mins;
 
-  MyMinutes({required this.mins});
+  const MyMinutes({required this.mins, super.key});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 5.0),
-      child: Container(
-        child: Center(
-          child: Text(
-            mins < 10 ? '0' + mins.toString() : mins.toString(),
-            style: TextStyle(
-              fontSize: 40,
-              // color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+      child: Center(
+        child: Text(
+          // mins < 10 ? '0' + mins.toString() : mins.toString(),
+          mins < 10 ? '0 ${mins.toString()}' : mins.toString(),
+          style: const TextStyle(
+            fontSize: 40,
+            // color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
