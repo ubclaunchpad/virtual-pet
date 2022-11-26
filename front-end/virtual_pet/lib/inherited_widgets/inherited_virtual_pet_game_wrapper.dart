9d2073fde4018058f97ab5cde180164c6241d955
Louie Weng <louieweng@outlook.com>
2022-11-26 09:10:48 -0800

    fixed formatting and logic in inherited game wrapper

--- front-end/virtual_pet/lib/inherited_widgets/inherited_virtual_pet_game_wrapper.dart
+++ front-end/virtual_pet/lib/inherited_widgets/inherited_virtual_pet_game_wrapper.dart
@@ -7,20 +7,10 @@ class InheritedVirtualPetGameWrapper extends InheritedWidget {
   const InheritedVirtualPetGameWrapper({
     required this.virtualPetGame, 
     required Widget child, 
-<<<<<<< HEAD
-    super.key
-  }) : super(child: child);
-
-  @override 
-  bool updateShouldNotify(InheritedWidget oldWidget) {
-    return this != oldWidget;
-  }
-=======
     super.key}) : super(child: child);
 
   @override 
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
->>>>>>> 93ac0d6 (refactored folder structure to reflect inherited widget change)
 
   static InheritedVirtualPetGameWrapper of(BuildContext context) {
     final InheritedVirtualPetGameWrapper? result = context
